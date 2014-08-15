88fd20ef59b7231df8b70e250ecb5e4a87a02cdc
Hendrik Kolver <hendrik.kolver@gmail.com>
2014-08-15 15:06:44 +0200

    Reran test with lena image

--- Experiments/HonoursResearchApplication/src/honoursresearchapplication/HonoursResearchApplication.java
+++ Experiments/HonoursResearchApplication/src/honoursresearchapplication/HonoursResearchApplication.java
@@ -15,7 +15,7 @@ public class HonoursResearchApplication {
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
        long start = System.currentTimeMillis();
        ImageHolder holder = new ImageHolder();
-       holder.extractImageData("tree.bmp");
+       holder.extractImageData("lena.bmp");
        
        SelfEmbed.selfEmbed(holder.getImage());
        System.out.println("Exiting After Successfull execution: "+ ((System.currentTimeMillis()-start)/1000.0)+"s");
