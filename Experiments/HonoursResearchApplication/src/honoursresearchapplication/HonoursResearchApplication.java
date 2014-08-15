1041199847f544256a35fe5849cb391e32b61958
Hendrik Kolver <hendrik.kolver@gmail.com>
2014-08-15 14:54:52 +0200

    added the tree

--- Experiments/HonoursResearchApplication/src/honoursresearchapplication/HonoursResearchApplication.java
+++ Experiments/HonoursResearchApplication/src/honoursresearchapplication/HonoursResearchApplication.java
@@ -15,7 +15,7 @@ public class HonoursResearchApplication {
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
        long start = System.currentTimeMillis();
        ImageHolder holder = new ImageHolder();
-       holder.extractImageData("lena.bmp");
+       holder.extractImageData("tree.bmp");
        
        SelfEmbed.selfEmbed(holder.getImage());
        System.out.println("Exiting After Successfull execution: "+ ((System.currentTimeMillis()-start)/1000.0)+"s");
